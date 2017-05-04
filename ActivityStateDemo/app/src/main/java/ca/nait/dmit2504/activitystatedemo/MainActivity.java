1c94a2dc2c16cbf491474c9d0286551dfbb4bf32
Dane Christenson <danechristenson@gmail.com>
2017-05-03 18:28:28 -0600

    Initial setup with overrides generated

---
+++ ActivityStateDemo/app/src/main/java/ca/nait/dmit2504/activitystatedemo/MainActivity.java
@@ -0,0 +1,39 @@
+package ca.nait.dmit2504.activitystatedemo;
+
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
