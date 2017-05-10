d3a79bdde8a3a399d1b6563f563e05a1eaef4649
Dane Christenson <danechristenson@gmail.com>
2017-05-10 17:48:55 -0600

    updated Gradle to add Volley Dependancy

--- ConnectingToARemoteServer/app/src/main/java/ca/nait/dmit/connectingtoaremoteserver/ReceiveDataActivity.java
+++ ConnectingToARemoteServer/app/src/main/java/ca/nait/dmit/connectingtoaremoteserver/ReceiveDataActivity.java
@@ -9,5 +9,7 @@ public class ReceiveDataActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive_data);
+
+
     }
 }
