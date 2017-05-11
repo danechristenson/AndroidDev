281bd13eb6fa51d085881c386fb35d4c3c361a99
Dane Christenson <danechristenson@gmail.com>
2017-05-10 18:41:39 -0600

    sending data fails every time

--- ConnectingToARemoteServer/app/src/main/java/ca/nait/dmit/connectingtoaremoteserver/ReceiveDataActivity.java
+++ ConnectingToARemoteServer/app/src/main/java/ca/nait/dmit/connectingtoaremoteserver/ReceiveDataActivity.java
@@ -2,6 +2,14 @@ package ca.nait.dmit.connectingtoaremoteserver;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class ReceiveDataActivity extends AppCompatActivity {
 
@@ -10,6 +18,25 @@ public class ReceiveDataActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive_data);
 
+        final TextView dataTextView = (TextView) findViewById(R.id.dataTextView);
 
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://www.youcode.ca/JSONServlet";
+
+        //Request a string response from the provided URL
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                //Display the response
+                dataTextView.setText("Response is: " + response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                dataTextView.setText("Error retrieving data from the server");
+            }
+        });
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 }
--- ConnectingToARemoteServer/app/src/main/java/ca/nait/dmit/connectingtoaremoteserver/SendDataActivity.java
+++ ConnectingToARemoteServer/app/src/main/java/ca/nait/dmit/connectingtoaremoteserver/SendDataActivity.java
@@ -4,6 +4,19 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SendDataActivity extends AppCompatActivity {
 
@@ -16,5 +29,41 @@ public class SendDataActivity extends AppCompatActivity {
     public void viewJitters(View view){
         Intent recieveDataIntent = new Intent(this, ReceiveDataActivity.class);
         startActivity(recieveDataIntent);
+    }
+
+    public void setData(View view) {
+        final TextView dataEditText = (TextView) findViewById(R.id.dataEditText);
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://www.youcode.ca/Jitter";
+
+        //Request a string response from the provided URL
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                //Display the response
+                Toast.makeText(SendDataActivity.this, response, Toast.LENGTH_LONG).show();
+                dataEditText.setText("");
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(SendDataActivity.this, "fail", Toast.LENGTH_SHORT).show();
+            }
+            }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                   // return super.getParams();
+                Map<String, String> params = new HashMap<>();
+                params.put("DATA", dataEditText.toString());
+                params.put("LOGIN_NAME", "Dane");
+                return params;
+
+
+            }
+        };
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
     }
 }
