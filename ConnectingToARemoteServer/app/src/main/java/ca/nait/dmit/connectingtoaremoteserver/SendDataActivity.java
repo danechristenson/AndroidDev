076a7840dd0e9909deadafe8ab8641f0a99e7fe0
Dane Christenson <danechristenson@gmail.com>
2017-05-12 17:54:24 -0600

    Added basic menu

--- ConnectingToARemoteServer/app/src/main/java/ca/nait/dmit/connectingtoaremoteserver/SendDataActivity.java
+++ ConnectingToARemoteServer/app/src/main/java/ca/nait/dmit/connectingtoaremoteserver/SendDataActivity.java
@@ -3,6 +3,8 @@ package ca.nait.dmit.connectingtoaremoteserver;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -66,4 +68,17 @@ public class SendDataActivity extends AppCompatActivity {
         queue.add(stringRequest);
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        //Create an instance of the menu inflater
+        MenuInflater inflater = getMenuInflater();
+        // inflate the menu
+        inflater.inflate(R.menu.main_menu, menu);
+        // return true if menu inflated ok
+        return true;
+   }
+
+
+
 }
