d52e43fd0c742ef81997e374da284fa7ea2f20e8
Dane Christenson <danechristenson@gmail.com>
2017-05-12 18:05:12 -0600

    changed viewjitters from button to a menu

--- ConnectingToARemoteServer/app/src/main/java/ca/nait/dmit/connectingtoaremoteserver/SendDataActivity.java
+++ ConnectingToARemoteServer/app/src/main/java/ca/nait/dmit/connectingtoaremoteserver/SendDataActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -79,6 +80,15 @@ public class SendDataActivity extends AppCompatActivity {
         return true;
    }
 
-
-
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.view_jitters:
+                Intent receiveDataIntent = new Intent(this, ReceiveDataActivity.class);
+                startActivity(receiveDataIntent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
