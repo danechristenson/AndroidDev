fb602f3b4ceb759cec0a4c2208f4c8ca64570cc7
Dane Christenson <danechristenson@gmail.com>
2017-05-12 18:55:38 -0600

    got list view working

--- ConnectingToARemoteServer/app/src/main/java/ca/nait/dmit/connectingtoaremoteserver/ReceiveDataActivity.java
+++ ConnectingToARemoteServer/app/src/main/java/ca/nait/dmit/connectingtoaremoteserver/ReceiveDataActivity.java
@@ -1,7 +1,10 @@
 package ca.nait.dmit.connectingtoaremoteserver;
 
+import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -11,24 +14,41 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
-public class ReceiveDataActivity extends AppCompatActivity {
+//step one modify activity class to extends ListActivity
+public class ReceiveDataActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_receive_data);
+       // setContentView(R.layout.activity_receive_data);
+//
+//        String[] hockeyTeams = new String[] {
+//                "Anaheim Ducks",
+//                "Nashville Predators",
+//                "Ottawa Senators",
+//                "Pittsburgh Penguins"
+//        };
+//        //STEP 3: create an adapter for the list data
+//        ListAdapter teamAdapter = new ArrayAdapter<String>(this, R.layout.list_view_item, hockeyTeams);
+//        setListAdapter(teamAdapter);
+
 
         final TextView dataTextView = (TextView) findViewById(R.id.dataTextView);
 
         RequestQueue queue = Volley.newRequestQueue(this);
         String url = "http://www.youcode.ca/JSONServlet";
 
-        //Request a string response from the provided URL
+        // Request a string response from the provided URL
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
-                //Display the response
-                dataTextView.setText("Response is: " + response);
+                // Display the response
+                // dataTextView.setText("Response is: " + response);
+                // STEP 3: create an adaptor for the list data
+                String[] responseArray = response.split("\r\n");
+                ListAdapter responseAdapter = new ArrayAdapter<String>(getApplicationContext(), R.layout.list_view_item, responseArray);
+// STEP 4: Associate the Adapter with the ListActivity
+                setListAdapter(responseAdapter);
             }
         }, new Response.ErrorListener() {
             @Override
