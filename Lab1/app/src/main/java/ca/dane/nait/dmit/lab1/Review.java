1155a4e09b0cb14f935f3255144c31b76bd0b368
Dane Christenson <danechristenson@gmail.com>
2017-05-29 19:17:46 -0600

    almost done Lab1 value returning null and not sure why

---
+++ Lab1/app/src/main/java/ca/dane/nait/dmit/lab1/Review.java
@@ -0,0 +1,13 @@
+package ca.dane.nait.dmit.lab1;
+
+/**
+ * Created by dchristenson5 on 5/29/2017.
+ */
+
+class Review {
+    String time;
+    String reviewer;
+    String category;
+    String nominee;
+    String review;
+}
--- Lab1/app/src/main/java/ca/dane/nait/dmit/lab1/ViewReviews.java
+++ Lab1/app/src/main/java/ca/dane/nait/dmit/lab1/ViewReviews.java
@@ -1,13 +1,20 @@
 package ca.dane.nait.dmit.lab1;
 
 import android.app.ListActivity;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -16,19 +23,23 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import java.util.ArrayList;
+
 public class ViewReviews extends ListActivity {
 
     protected String categoryRadioSelection = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_view_reviews);
         Bundle extras = getIntent().getExtras();
-        if(extras !=null) {
+        if (extras != null) {
             categoryRadioSelection = extras.getString("radioValue");
         }
 
-        final TextView dataTextView = (TextView) findViewById(R.id.reviewsTextView);
+
+        final ListView dataTextView = (ListView) findViewById(R.id.reviews_list_view);
 
         RequestQueue queue = Volley.newRequestQueue(this);
 
@@ -42,14 +53,55 @@ public class ViewReviews extends ListActivity {
                 // dataTextView.setText("Response is: " + response);
                 // STEP 3: create an adaptor for the list data
                 String[] responseArray = response.split("\r\n");
-                ListAdapter responseAdapter = new ArrayAdapter<String>(getApplicationContext(), R.layout.list_view_item, responseArray);
+                ListAdapter responseAdapter = new ArrayAdapter<>(getApplicationContext(), R.layout.list_view_item, responseArray);
+               final ArrayList<Review> reviews = new ArrayList<>();
+                for (int i = 0; i < responseArray.length; i++) {
+                    try {
+                        Review currentReview = new Review();
+
+                        currentReview.time = responseArray[i];
+                        currentReview.reviewer = responseArray[i + 1];
+                        currentReview.category = responseArray[i + 2];
+                        currentReview.nominee = responseArray[i + 3];
+                        currentReview.review = responseArray[i + 4];
+
+                        reviews.add(currentReview);
+                    } catch (Exception ex) {
+                        Toast.makeText(ViewReviews.this, "something broke", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                ArrayAdapter<Review> reviewList =  new ArrayAdapter<Review>(getApplicationContext(), -1, reviews) {
+                    @NonNull
+                    @Override
+                    public View getView(int position, @Nullable View convertView,
+                                        @NonNull ViewGroup parent) {
+                        LayoutInflater inflater = (LayoutInflater)  ViewReviews.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                        View inflatedView = inflater.inflate(R.layout.list_view_item, null);
+                        TextView timeTextView = (TextView) inflatedView.findViewById(R.id.time_textview);
+                        timeTextView.setText(reviews.get(position).time);
+                        TextView reviewerTextView = (TextView) inflatedView.findViewById(R.id.review_textview);
+                        reviewerTextView.setText(reviews.get(position).reviewer);
+                        TextView categoryTextView = (TextView) inflatedView.findViewById(R.id.category_textview);
+                        categoryTextView.setText(reviews.get(position).time);
+                        TextView nomineeTextView = (TextView) inflatedView.findViewById(R.id.nominee_textview);
+                        nomineeTextView.setText(reviews.get(position).time);
+                        TextView reviewTextView = (TextView) inflatedView.findViewById(R.id.review_textview);
+                        reviewTextView.setText(reviews.get(position).time);
+
+                        return inflatedView;
+                    }
+                };
+
+                ListView reviewsListView = (ListView) findViewById(R.id.reviews_list_view);
+                reviewsListView.setAdapter(reviewList); //TODO:(dane) Find out why this is returning NULL
 // STEP 4: Associate the Adapter with the ListActivity
                 setListAdapter(responseAdapter);
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                dataTextView.setText("Error retrieving data from the server");
+                //dataTextView.setText("Error retrieving data from the server");
             }
         });
         // Add the request to the RequestQueue.
