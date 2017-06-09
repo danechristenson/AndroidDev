a06c143737ace9b3029796e420d91a218a47892c
Dane Christenson <danechristenson@gmail.com>
2017-06-08 22:28:16 -0600

    got everything working I think, ListView returns null and breaks though

--- Lab1/app/src/main/java/ca/dane/nait/dmit/lab1/ViewReviews.java
+++ Lab1/app/src/main/java/ca/dane/nait/dmit/lab1/ViewReviews.java
@@ -47,7 +47,7 @@ public class ViewReviews extends ListActivity {
         // Request a string response from the provided URL
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
             @Override
-            public void onResponse(String response) { //TODO:(dane) why is this being skipped?
+            public void onResponse(String response) {
                 // Display the response
                 // dataTextView.setText("Response is: " + response);
                 // STEP 3: create an adaptor for the list data
@@ -56,16 +56,20 @@ public class ViewReviews extends ListActivity {
 
                 //ListAdapter responseAdapter = new ArrayAdapter<>(getApplicationContext(), R.layout.list_view_item, responseArray);
                ArrayList<Review> reviews = new ArrayList<>();
-                for (int i = 0; i < responseArray.length; i++) {
+                for (int i = 0; i < responseArray.length; i+=5) {
                     try {
-                        String currentLineText = responseArray[i]; //shouldnt need this as each thing is added to i with a new line
+                        String time = responseArray[i];
+                        String reviewer = responseArray[i+1];
+                        String category = responseArray[i+2];
+                        String nominee = responseArray[i+3];
+                        String review = responseArray[i+4];
 
                         Review currentReview = new Review();
-                        currentReview.time = responseArray[i];
-                        currentReview.reviewer = responseArray[i + 1];
-                        currentReview.category = responseArray[i + 2];
-                        currentReview.nominee = responseArray[i + 3];
-                        currentReview.review = responseArray[i + 4];
+                        currentReview.time = time; //responseArray[i];
+                        currentReview.reviewer = reviewer;//responseArray[i + 1];
+                        currentReview.category = category;//responseArray[i + 2];
+                        currentReview.nominee = nominee;//responseArray[i + 3];
+                        currentReview.review = review;//responseArray[i + 4];
 
                         reviews.add(currentReview);
                     } catch (Exception ex) {
@@ -73,8 +77,8 @@ public class ViewReviews extends ListActivity {
                     }
                 }
 
-                ListView reviewsListView = (ListView) findViewById(R.id.reviews_list_view);
-                ListViewAdapter adapter = new ListViewAdapter(getApplicationContext(),reviews);
+                ListView reviewsListView = (ListView) findViewById(R.id.reviews_list_view); //FIXME(dane): why does this come back null?
+                ListViewAdapter adapter = new ListViewAdapter(getApplicationContext(), reviews);
                 reviewsListView.setAdapter(adapter);
 // STEP 4: Associate the Adapter with the ListActivity
 
