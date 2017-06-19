36dc9232cdc0a26f943293eba6b9ca1e9b3a63d4
Dane Christenson <danechristenson@gmail.com>
2017-06-19 17:51:49 -0600

    added Track class
    stores trackName and artistName, generated getters and setters

--- Itunes/app/src/main/java/ca/dane/nait/dmit/itunes/Track.java
+++ Itunes/app/src/main/java/ca/dane/nait/dmit/itunes/Track.java
@@ -5,6 +5,25 @@ package ca.dane.nait.dmit.itunes;
  */
 
 public class Track {
+
+
     private String trackName;
     private String artistName;
+
+    public String getTrackName() {
+        return trackName;
+    }
+
+    public void setTrackName(String trackName) {
+        this.trackName = trackName;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
 }
