f8e3c697474e05c76678662c1db42d1171b54b0f
Dane Christenson <danechristenson@gmail.com>
2017-06-19 18:22:10 -0600

    added constructor for the TrackAdapter

--- Itunes/app/src/main/java/ca/dane/nait/dmit/itunes/TrackAdapter.java
+++ Itunes/app/src/main/java/ca/dane/nait/dmit/itunes/TrackAdapter.java
@@ -1,15 +1,30 @@
 package ca.dane.nait.dmit.itunes;
 
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+
+import java.util.List;
 
 /**
  * Created by dchristenson5 on 6/19/2017.
  */
 
 public class TrackAdapter extends BaseAdapter{
+    private Context mContext;
+    private List<Track> mDataSource;
+    private LayoutInflater mInflater;
+
+    public TrackAdapter(Context mContext, List<Track> mDataSource) {
+        this.mContext = mContext;
+        this.mDataSource = mDataSource;
+        mInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+
     @Override
     public int getCount() {
         return 0;
