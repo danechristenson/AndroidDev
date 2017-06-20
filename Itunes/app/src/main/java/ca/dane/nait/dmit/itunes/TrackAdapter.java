8e224406ed5905c2a36b94cc399513da3a375d9a
Dane Christenson <danechristenson@gmail.com>
2017-06-19 18:19:28 -0600

    Created trackAdapter class to use the listview

---
+++ Itunes/app/src/main/java/ca/dane/nait/dmit/itunes/TrackAdapter.java
@@ -0,0 +1,32 @@
+package ca.dane.nait.dmit.itunes;
+
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by dchristenson5 on 6/19/2017.
+ */
+
+public class TrackAdapter extends BaseAdapter{
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
