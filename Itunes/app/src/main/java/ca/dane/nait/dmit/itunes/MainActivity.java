d68ffe6fe603784f2625ce16da6c67a050ea440d
Dane Christenson <danechristenson@gmail.com>
2017-06-19 18:16:03 -0600

    working to receive the json

--- Itunes/app/src/main/java/ca/dane/nait/dmit/itunes/MainActivity.java
+++ Itunes/app/src/main/java/ca/dane/nait/dmit/itunes/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Toast.makeText(MainActivity.this, "Error Processing json", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Error processing json", Toast.LENGTH_SHORT).show();
             }
         });
 
