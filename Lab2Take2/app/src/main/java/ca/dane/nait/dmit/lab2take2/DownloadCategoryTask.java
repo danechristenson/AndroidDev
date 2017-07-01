a4f74405f12db13a4bb2dcb86dc3c284b78504f1
Dane Christenson <danechristenson@gmail.com>
2017-06-30 18:54:26 -0600

    Got the category selector base working

--- Lab2Take2/app/src/main/java/ca/dane/nait/dmit/lab2take2/DownloadCategoryTask.java
+++ Lab2Take2/app/src/main/java/ca/dane/nait/dmit/lab2take2/DownloadCategoryTask.java
@@ -1,17 +1,28 @@
 package ca.dane.nait.dmit.lab2take2;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.util.Log;
+import android.view.View;
 import android.view.ViewParent;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import org.apache.commons.io.IOUtils;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 /**
  * Created by dchristenson5 on 6/28/2017.
@@ -30,10 +41,24 @@ public class DownloadCategoryTask extends AsyncTask<String, Void, List<String>> 
     protected List<String> doInBackground(String... params) {
         String urlString = params[0];
         HttpURLConnection connection = null;
+        BufferedReader in = null;
+        ArrayList jitters = new ArrayList();
         try {
             URL url = new URL(urlString);
             connection = (HttpURLConnection) url.openConnection();
             InputStream inputStream = new BufferedInputStream(connection.getInputStream());
+
+            in = new BufferedReader(new InputStreamReader(inputStream));
+
+            String line = "";
+            String NL = System.getProperty("line.separator");
+
+            while((line = in.readLine()) != null)
+            {
+                jitters.add(line);
+            }
+            in.close();
+
         } catch (Exception ex){
             Log.e("Network Error: ", ex.getMessage());
         } finally {
@@ -41,17 +66,24 @@ public class DownloadCategoryTask extends AsyncTask<String, Void, List<String>> 
                 connection.disconnect();
             }
         }
-        return null ;
+
+
+
+        return jitters ;
     }
     @Override
-    protected void onPostExecute(List<String> string) {
-        ArrayAdapter<String> results = new ArrayAdapter<String>(mContext, android.R.layout.simple_list_item_1, string);
-        for (int i = 0; i < string.size(); ++i){
-            results.add(string.get(i));
-        }
-
-
+    protected void onPostExecute(List<String> data) {
+        final List<String> newdata = data;
+        ArrayAdapter<String> results = new ArrayAdapter<String>(mContext, android.R.layout.simple_list_item_1, data);
         mListView.setAdapter(results);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent();
+                intent.putExtra("category", newdata.get(position));
+                Toast.makeText(mContext, newdata.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 }
--- Lab2Take2/app/src/main/java/ca/dane/nait/dmit/lab2take2/PickCategory.java
+++ Lab2Take2/app/src/main/java/ca/dane/nait/dmit/lab2take2/PickCategory.java
@@ -1,8 +1,12 @@
 package ca.dane.nait.dmit.lab2take2;
 
 import android.app.LauncherActivity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -15,11 +19,14 @@ public class PickCategory extends AppCompatActivity {
         setContentView(R.layout.activity_pick_category);
 
         ListView categoryListView = (ListView) findViewById(R.id.pick_category_ListView);
-        new DownloadCategoryTask(this, categoryListView).execute("http://www.youcode.ca/Lab02Servlet?Service=categories");
+        try {
+            new DownloadCategoryTask(PickCategory.this, categoryListView).execute("http://www.youcode.ca/Lab02Servlet?Service=categories");
+        }catch (Exception e){
+            Log.i("broken", e.getMessage());
+        }
 
 
 
-      // categoryListView.setItemOnClick
     }
 
 
