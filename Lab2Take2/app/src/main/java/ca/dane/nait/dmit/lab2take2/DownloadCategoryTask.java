4fec2b52b7988fc47295fe51d1950dc0e388c27c
Dane Christenson <danechristenson@gmail.com>
2017-06-30 19:14:55 -0600

    got rid of extra imports

--- Lab2Take2/app/src/main/java/ca/dane/nait/dmit/lab2take2/DownloadCategoryTask.java
+++ Lab2Take2/app/src/main/java/ca/dane/nait/dmit/lab2take2/DownloadCategoryTask.java
@@ -5,13 +5,10 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.util.Log;
 import android.view.View;
-import android.view.ViewParent;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
-
-import org.apache.commons.io.IOUtils;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
@@ -21,8 +18,6 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-
-import static java.nio.charset.StandardCharsets.UTF_8;
 
 /**
  * Created by dchristenson5 on 6/28/2017.
